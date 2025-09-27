1830f23cf08e80cd450a23e9c5ec6ce9ea902cb6
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-27 03:28:47 +0000

    add fastapi two listening ports uses

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from threading import Thread
+
 from app import app
-
-
+import threading
+import asyncio
 import uvicorn
 
+async def run_server(port):
+    config = uvicorn.Config("app.app:app", host="0.0.0.0", port=port, reload=False)
+    server = uvicorn.Server(config)
+    await server.serve()
+
+async def main():
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(run_server(8000))
+        tg.create_task(run_server(9000))
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
+    asyncio.run(main())
+
+    # uvicorn.run("app.app:app", host="0.0.0.0", port=8000, reload=True)
