0ab6042844ddb0769256c90bfb0cc96f06ab2c2a
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-27 06:58:38 +0000

    add anyio usage

--- app/routes/about.py
+++ app/routes/about.py
@@ -1,9 +1,13 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-
+import anyio
 router = APIRouter()
 
 
 @router.get('/about')
-def about():
+async def about():
+    print(f'about is processing ...')
+    task = anyio.get_current_task()
+    print(f"Current task: {task!r}")
+    print(f"Task name: {task.name}")
     return 'v1'
--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,9 +1,17 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
-
+import anyio
 router = APIRouter()
 
 
 @router.get('/admin')
-def about():
+async def about():
+    while True:
+        # breakpoint()
+        print("admin is processing ...", flush=True)
+        task = anyio.get_current_task()
+        print(f"Current task: {task!r}")
+        print(f"Task name: {task.name}")
+        await anyio.sleep(1)
+
     return 'admin'
