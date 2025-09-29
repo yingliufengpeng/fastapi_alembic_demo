3e6d4dfa3bc3be409b886f378235ccc618673bef
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-29 04:49:53 +0000

    add scheduler week task

--- app/cron.py
+++ app/cron.py
@@ -1,4 +1,5 @@
 import time
+import anyio
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
@@ -10,6 +11,13 @@ scheduler = None
 def get_scheduler():
     return scheduler
 
+
+async def process():
+    print(f'running process ...', flush=True)
+
+def wrapper():
+    anyio.run(process)
+
 @asynccontextmanager
 async def cron_life_span(app: FastAPI):
     # Startup 逻辑
@@ -17,6 +25,17 @@ async def cron_life_span(app: FastAPI):
     print("cron_life_span startup", flush=True)
     scheduler = BackgroundScheduler()
     scheduler.start()
+    # scheduler.add_job(wrapper, 'interval', seconds=3)
+    # 添加任务：每周凌晨3点执行
+    scheduler.add_job(
+        wrapper,
+        trigger='cron',
+        day_of_week='mon',  # 可改为 '0' 或 'mon' 表示周一
+        hour=3,
+        minute=0,
+        second=0,
+        id='weekly_job'
+    )
     yield
     # Shutdown 逻辑
     scheduler.shutdown()
